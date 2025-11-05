a32149191e7743d1c7978e853c2ff4d510238b3e
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-11-05 11:07:48 +0700

    menambahkan code di hotel

---
+++ models/hotel.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const Hotel = sequelize.define('hotel', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Tipe_Kamar: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Kapasitas_Tamu: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    Lantai: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    Fasilitas: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    Tanggal_Pesan: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    }
+  }, {
+    tableName: 'hotel',
+    freezeTableName: true,
+    timestamps: true,
+  });
+
+  return Hotel;
+};
